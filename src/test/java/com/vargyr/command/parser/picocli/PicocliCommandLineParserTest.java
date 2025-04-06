d263f69f588832e09c52aa17ef9252bf00ff172c
Jesse Sandvik <sandvikjesse@gmail.com>
2025-04-06 18:30:57 -0400

    adding unit tests for picocli command line parser, subcommand and nested subcommand

--- src/test/java/com/vargyr/command/parser/picocli/PicocliCommandLineParserTest.java
+++ src/test/java/com/vargyr/command/parser/picocli/PicocliCommandLineParserTest.java
@@ -11,6 +11,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
+
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -21,6 +23,10 @@ public class PicocliCommandLineParserTest {
     private final CommandExecutionErrorManager mockErrorManager = mock(CommandExecutionErrorManager.class);
     private final VgrCommand mockCommand = mock(VgrCommand.class);
     private final VgrCommandMetadata mockMetadata = mock(VgrCommandMetadata.class);
+    private final VgrCommand mockSubcommand = mock(VgrCommand.class);
+    private final VgrCommandMetadata mockSubcommandMetadata = mock(VgrCommandMetadata.class);
+    private final VgrCommand mockNestedSubcommand = mock(VgrCommand.class);
+    private final VgrCommandMetadata mockNestedSubcommandMetadata = mock(VgrCommandMetadata.class);
 
     @BeforeEach
     public void setUp() {
@@ -117,11 +123,104 @@ public class PicocliCommandLineParserTest {
     }
 
     @Test
+    public void testParseWhenNoArgumentsAndCommandExecutesWithoutArguments() {
+        when(mockMetadata.getName()).thenReturn("test-command");
+        when(mockMetadata.getExecutesWithoutArguments()).thenReturn(true);
+
+        String[] arguments = {};
+        when(mockCommandExecution.getOriginalArguments()).thenReturn(arguments);
+        when(mockCommandExecution.getRootCommand()).thenReturn(mockCommand);
+        doNothing().when(mockCommandExecution).setInvokedCommand(eq(mockCommand));
+        parser.parse();
+        verify(mockCommandExecution, times(1)).setInvokedCommand(eq(mockCommand));
+    }
+
+    @Test
     public void testParseWhenNoArgumentsAndCommandDoesNotExecuteWithoutArguments() {
         when(mockMetadata.getName()).thenReturn("test-command");
         when(mockMetadata.getExecutesWithoutArguments()).thenReturn(false);
 
         String[] arguments = {};
+        when(mockCommandExecution.getOriginalArguments()).thenReturn(arguments);
+        doNothing().when(mockCommandExecution).setState(eq(CommandExecutionState.END));
+        doNothing().when(mockCommandExecution).setExitCode(eq(0));
+        parser.parse();
+        verify(mockCommandExecution, times(1)).setState(eq(CommandExecutionState.END));
+        verify(mockCommandExecution, times(1)).setExitCode(eq(0));
+    }
+
+    @Test
+    public void testParseWhenSubcommandWithNoArgumentsAndSubcommandExecutesWithoutArguments() {
+        when(mockMetadata.getName()).thenReturn("test-command");
+        when(mockCommand.getSubcommands()).thenReturn(List.of(mockSubcommand));
+
+        when(mockSubcommand.getMetadata()).thenReturn(mockSubcommandMetadata);
+        when(mockSubcommandMetadata.getName()).thenReturn("test-subcommand");
+        when(mockSubcommandMetadata.getExecutesWithoutArguments()).thenReturn(true);
+
+        when(mockCommandExecution.getRootCommand()).thenReturn(mockCommand);
+        doNothing().when(mockCommandExecution).setInvokedCommand(eq(mockSubcommand));
+
+        String[] arguments = {"test-subcommand"};
+        when(mockCommandExecution.getOriginalArguments()).thenReturn(arguments);
+        parser.parse();
+        verify(mockCommandExecution, times(1)).setInvokedCommand(eq(mockSubcommand));
+    }
+
+    @Test
+    public void testParseWhenSubcommandWithNoArgumentsAndSubcommandDoesNotExecuteWithoutArguments() {
+        when(mockMetadata.getName()).thenReturn("test-command");
+        when(mockCommand.getSubcommands()).thenReturn(List.of(mockSubcommand));
+
+        when(mockSubcommand.getMetadata()).thenReturn(mockSubcommandMetadata);
+        when(mockSubcommandMetadata.getName()).thenReturn("test-subcommand");
+        when(mockSubcommandMetadata.getExecutesWithoutArguments()).thenReturn(false);
+
+        String[] arguments = {"test-subcommand"};
+        when(mockCommandExecution.getOriginalArguments()).thenReturn(arguments);
+        doNothing().when(mockCommandExecution).setState(eq(CommandExecutionState.END));
+        doNothing().when(mockCommandExecution).setExitCode(eq(0));
+        parser.parse();
+        verify(mockCommandExecution, times(1)).setState(eq(CommandExecutionState.END));
+        verify(mockCommandExecution, times(1)).setExitCode(eq(0));
+    }
+
+    @Test
+    public void testParseWhenNestedSubcommandWithNoArgumentsAndNestedSubcommandExecutesWithoutArguments() {
+        when(mockMetadata.getName()).thenReturn("test-command");
+        when(mockCommand.getSubcommands()).thenReturn(List.of(mockSubcommand));
+
+        when(mockSubcommand.getMetadata()).thenReturn(mockSubcommandMetadata);
+        when(mockSubcommandMetadata.getName()).thenReturn("test-subcommand");
+        when(mockSubcommand.getSubcommands()).thenReturn(List.of(mockNestedSubcommand));
+
+        when(mockNestedSubcommand.getMetadata()).thenReturn(mockNestedSubcommandMetadata);
+        when(mockNestedSubcommandMetadata.getName()).thenReturn("test-nested-subcommand");
+        when(mockNestedSubcommandMetadata.getExecutesWithoutArguments()).thenReturn(true);
+
+        when(mockCommandExecution.getRootCommand()).thenReturn(mockCommand);
+        doNothing().when(mockCommandExecution).setInvokedCommand(eq(mockNestedSubcommand));
+
+        String[] arguments = {"test-subcommand", "test-nested-subcommand"};
+        when(mockCommandExecution.getOriginalArguments()).thenReturn(arguments);
+        parser.parse();
+        verify(mockCommandExecution, times(1)).setInvokedCommand(eq(mockNestedSubcommand));
+    }
+
+    @Test
+    public void testParseWhenNestedSubcommandWithNoArgumentsAndNestedSubcommandDoesNotExecuteWithoutArguments() {
+        when(mockMetadata.getName()).thenReturn("test-command");
+        when(mockCommand.getSubcommands()).thenReturn(List.of(mockSubcommand));
+
+        when(mockSubcommand.getMetadata()).thenReturn(mockSubcommandMetadata);
+        when(mockSubcommandMetadata.getName()).thenReturn("test-subcommand");
+        when(mockSubcommand.getSubcommands()).thenReturn(List.of(mockNestedSubcommand));
+
+        when(mockNestedSubcommand.getMetadata()).thenReturn(mockNestedSubcommandMetadata);
+        when(mockNestedSubcommandMetadata.getName()).thenReturn("test-nested-subcommand");
+        when(mockNestedSubcommandMetadata.getExecutesWithoutArguments()).thenReturn(false);
+
+        String[] arguments = {"test-subcommand", "test-nested-subcommand"};
         when(mockCommandExecution.getOriginalArguments()).thenReturn(arguments);
         doNothing().when(mockCommandExecution).setState(eq(CommandExecutionState.END));
         doNothing().when(mockCommandExecution).setExitCode(eq(0));
